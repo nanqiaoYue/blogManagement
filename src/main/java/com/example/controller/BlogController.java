17f24cb11e7e4e586d41ae734871ce6a98a78d53
nanqiaoYue <31209616+nanqiaoYue@users.noreply.github.com>
2017-10-08 22:04:14 +0800

    Initial commit

---
+++ src/main/java/com/example/controller/BlogController.java
@@ -0,0 +1,82 @@
+package com.example.controller;
+
+import com.example.model.BlogEntity;
+import com.example.model.UserEntity;
+import com.example.repository.BlogRepository;
+import com.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+/**
+ * Created by L on 2016/10/8.
+ */
+@Controller
+public class BlogController {
+
+    @Autowired
+    BlogRepository blogRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    // 查看所有博文
+    @RequestMapping(value = "/admin/blogs", method = RequestMethod.GET)
+    public String showBlogs(ModelMap modelMap) {
+        List<BlogEntity> blogList = blogRepository.findAll();
+        modelMap.addAttribute("blogList", blogList);
+        return "admin/blogs";
+    }
+    // 添加博文
+    @RequestMapping(value = "/admin/blogs/add", method = RequestMethod.GET)
+    public String addBlog(ModelMap modelMap) {
+        List<UserEntity> userList = userRepository.findAll();
+        modelMap.addAttribute("userList", userList);
+        return "admin/addBlog";
+    }
+
+    // 添加博文，POST请求
+    @RequestMapping(value = "/admin/blogs/addP", method = RequestMethod.POST)
+    public String addBlogPost(@ModelAttribute("blog") BlogEntity blogEntity) {
+        System.out.println(blogEntity.getTitle());
+        System.out.println(blogEntity.getUserByUserId().getNickname());
+        blogRepository.saveAndFlush(blogEntity);
+        return "redirect:/admin/blogs";
+    }
+
+    @RequestMapping("/admin/blogs/show/{id}")
+    public String showBlog(@PathVariable("id") int id, ModelMap modelMap) {
+        BlogEntity blog = blogRepository.findOne(id);
+        modelMap.addAttribute("blog", blog);
+        return "admin/blogDetail";
+    }
+
+    @RequestMapping("/admin/blogs/update/{id}")
+    public String updateBlog(@PathVariable("id") int id, ModelMap modelMap) {
+        BlogEntity blog = blogRepository.findOne(id);
+        List<UserEntity> userList = userRepository.findAll();
+        modelMap.addAttribute("blog", blog);
+        modelMap.addAttribute("userList", userList);
+        return "admin/updateBlog";
+    }
+
+    @RequestMapping(value = "/admin/blogs/updateP", method = RequestMethod.POST)
+    public String updateBlogP(@ModelAttribute("blogP") BlogEntity blogEntity) {
+        blogRepository.updateBlog(blogEntity.getTitle(), blogEntity.getUserByUserId().getId(),
+                blogEntity.getContent(), blogEntity.getPubDate(), blogEntity.getId());
+        blogRepository.flush();
+        return "redirect:/admin/blogs";
+    }
+
+    @RequestMapping("/admin/blogs/delete/{id}")
+    public String deleteBlog(@PathVariable("id") int id) {
+        blogRepository.delete(id);
+        blogRepository.flush();
+        return "redirect:/admin/blogs";
+    }
+}
